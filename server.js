0677ab1af13cbbc3c058b8f8f7c7c7f2302e1d56
Caroline Wanjiku <Caroline Wanjiku>
2017-12-21 17:09:53 +0300

    bg(heroku)Modify server.js

--- server.js
+++ server.js
@@ -1,36 +1,5 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
-var serveStatic = require('serve-static');
-var sendMail = require('./src/api/sendmail');
-var bodyParser = require('body-parser');
-var app = new (require('express'))()
+var express = require('express');
+var app = express();
 
-var port = process.env.WEBPACK_PORT || 3000
-
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
-
-// parse application/json
-app.use(bodyParser.json());
-
-    // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
-
-
-
-
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
-})
+app.use(express.static(__dirname +'./../../')); //serves the index.html
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
